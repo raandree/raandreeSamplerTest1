42cf7a012a58a4a80d5fad36cd03971cd5a902bd
Raimund Andree <r.andree@live.com>
2025-06-07 15:12:03 +0200

    fixes

--- .build/GitVersion.ps1
+++ .build/GitVersion.ps1
@@ -16,24 +16,6 @@ task GitVersion -if (Get-Command -Name dotnet-gitversion.exe, gitversion.exe -Er
         return
     }
 
-    #dotnetgitversioninstall: Sepertate install task only on Az Agents without GitVersion not installed
-
-    if (Get-Command -Name dotnet-gitversion -ErrorAction SilentlyContinue)
-    {
-        Write-Host 'dotnet-gitversion is already installed.'
-    }
-    else
-    {
-        Write-Host 'Installing dotnet-gitversion...'
-        dotnet tool install --global GitVersion.Tool
-        Write-Host 'done.'
-    }
-
-    #Write-Host 'Installing GitVersion.Tool...' -NoNewline
-    #dotnet tool install --global GitVersion.Tool
-    #Write-Host 'done.'
-
-    #either dotnet-gitversion or gitversion
     $gitVersionObject = & $command
     Write-Host -------------- GitVersion Outout --------------
     $gitVersionObject | Write-Host
@@ -46,7 +28,6 @@ task GitVersion -if (Get-Command -Name dotnet-gitversion.exe, gitversion.exe -Er
         Write-Host -Object "##vso[task.setvariable variable=$($_.Name);]$($_.Value)"
     }
 
-    #Check the gitversion modes (CD) / how gitversion calculates the prerelease version
     $lastTag = git describe --tags --abbrev=0
     $lastTag -match '^v(?<Version>(\d(\.)?){3})(-preview(?<PreviewReleaseNumber>\d{4}))?' | Out-Null
     $lastVersion = $matches['Version']
@@ -74,17 +55,11 @@ task GitVersion -if (Get-Command -Name dotnet-gitversion.exe, gitversion.exe -Er
     }
 
     $versionString = -join $versionElements
-
-    Write-Host -Object "Writing version string '$versionString' to build variable 'NuGetVersionV2'."
-    #Write-Host -Object "##vso[task.setvariable variable=NuGetVersionV2;]$($versionString)"
-    Write-Host -Object "##vso[task.setvariable variable=ModuleVersion;]$($versionString)"
-    $env:ModuleVersion = $versionString
-    $global:ModuleVersion = $versionString
     [System.Environment]::SetEnvironmentVariable('ModuleVersion', $versionString, 'Process')
 
-    Write-Host -Object "Updating build number to '$versionString'."
-    Write-Host -Object "##vso[build.updatebuildnumber]$($versionString)"
-}
+    Write-Host "Writing version string '$versionString' to build / environment variable 'VersionString'."
+    Write-Host "##vso[task.setvariable variable=VersionString;]$($versionString)"
 
-#$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
-#ModuleVersion: $(NuGetVersionV2)
+    Write-Host "Updating build number to '$versionString'."
+    Write-Host "##vso[build.updatebuildnumber]$($versionString)"
+}
